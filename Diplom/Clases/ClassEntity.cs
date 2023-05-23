4f825357c67fb3a5fefac5dd3dc026290b58a0d7
Fanat4ebyrekov <81490021+Fanat4ebyrekov@users.noreply.github.com>
2023-05-24 01:24:00 +0300

    Добавьте файлы проекта.

---
+++ Diplom/Clases/ClassEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Diplom.BD;
+
+namespace Diplom.Clases
+{
+    internal class ClassEntity
+    {
+        public static BD.Entities context = new BD.Entities();
+    }
+}
---
+++ Diplom/Pages/WorkerPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static Diplom.Clases.ClassEntity;
+using Diplom.BD;
+using Diplom.Windows;
+
+namespace Diplom.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkerPage.xaml
+    /// </summary>
+    public partial class WorkerPage : Page
+    {
+        public WorkerPage(Windows.MainWindow mainWindow)
+        {
+            InitializeComponent();
+            AllWorker.ItemsSource = context.vw_Post.ToList();
+        }
+
+        private void btnMoreInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MoreInfoWindow moreInfoWindow = new MoreInfoWindow();
+            moreInfoWindow.Show();
+        }
+    }
+}
